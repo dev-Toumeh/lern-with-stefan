a85349cf01450a887f6247fa267392e79c6ce7ef
toumeh <dev.toumeh@gmail.com>
2024-03-03 10:46:41 +0100

    fixing the project after ssd fehler part1

--- src/config/system/settings.php
+++ src/config/system/settings.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'BE' => [
-        'debug' => true,
-        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$Tm5OWmlSeTU0R2EydlhPSg$pU3Y7OYvawe18ilCcuk01zrXljCUTCb2nSnSFQgYQSA',
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$WlVRaDY4dUh3dks1dS44VA$lu2EGr9UuZgWf31z5oYdu8XwW1kRTAntXuB+H2VXa20',
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
@@ -15,20 +15,13 @@ return [
                 'dbname' => 'typo3',
                 'driver' => 'pdo_mysql',
                 'host' => 'db',
-                'password' => '222568',
+                'password' => '1234',
                 'port' => 3306,
                 'tableoptions' => [
                     'charset' => 'utf8mb4',
                     'collate' => 'utf8mb4_unicode_ci',
                 ],
                 'user' => 'admin',
-            ],
-        ],
-    ],
-    'EXTCONF' => [
-        'lang' => [
-            'availableLanguages' => [
-                'de',
             ],
         ],
     ],
@@ -46,68 +39,17 @@ return [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
         ],
-        'styleguide' => [
-            'boolean_1' => '0',
-            'boolean_2' => '1',
-            'boolean_3' => '',
-            'boolean_4' => '0',
-            'color_1' => 'black',
-            'color_2' => '#000000',
-            'color_3' => '000000',
-            'color_4' => '',
-            'compat_default_1' => 'value',
-            'compat_default_2' => '',
-            'compat_input_1' => 'value',
-            'compat_input_2' => '',
-            'int_1' => '1',
-            'int_2' => '',
-            'int_3' => '-100',
-            'int_4' => '2',
-            'intplus_1' => '1',
-            'intplus_2' => '',
-            'intplus_3' => '2',
-            'nested' => [
-                'input_1' => 'aDefault',
-                'input_2' => '',
-            ],
-            'offset_1' => 'x,y',
-            'offset_2' => 'x',
-            'offset_3' => ',y',
-            'offset_4' => '',
-            'options_1' => 'default',
-            'options_2' => 'option_2',
-            'options_3' => '',
-            'predefined' => [
-                'boolean_1' => '1',
-                'int_1' => '42',
-            ],
-            'small_1' => 'value',
-            'small_2' => '',
-            'string_1' => 'value',
-            'string_2' => '',
-            'user_1' => '0',
-            'wrap_1' => 'value',
-            'wrap_2' => '',
-        ],
     ],
     'FE' => [
         'cacheHash' => [
             'enforceValidation' => true,
         ],
-        'debug' => true,
+        'debug' => false,
         'disableNoCacheParameter' => true,
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
         ],
-    ],
-    'GFX' => [
-        'processor' => 'ImageMagick',
-        'processor_allowTemporaryMasksAsPng' => '0',
-        'processor_colorspace' => 'RGB',
-        'processor_effects' => false,
-        'processor_enabled' => '1',
-        'processor_path' => '/usr/bin/',
     ],
     'LOG' => [
         'TYPO3' => [
@@ -116,7 +58,7 @@ return [
                     'writerConfiguration' => [
                         'notice' => [
                             'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                'disabled' => false,
+                                'disabled' => true,
                             ],
                         ],
                     ],
@@ -159,15 +101,15 @@ return [
                 ],
             ],
         ],
-        'devIPmask' => '*',
-        'displayErrors' => 1,
-        'encryptionKey' => '0b6e2cc4cb9c607eac8689891d14fe294acd471dda700b13b2da160ad9e62df389af504532802bbc6729bb1a50bae360',
-        'exceptionalErrors' => 12290,
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '8e9f786127c4c757ca494875d19adc5f2a8762b9cc70ff37fe145837696a37810ce5452d0bcfce8d72a13b2ee88af50b',
+        'exceptionalErrors' => 4096,
         'features' => [
             'security.backend.enforceContentSecurityPolicy' => true,
             'security.usePasswordPolicyForFrontendUsers' => true,
         ],
-        'sitename' => 'mywebsite.local',
+        'sitename' => 'New TYPO3 site',
         'systemMaintainers' => [
             1,
         ],
